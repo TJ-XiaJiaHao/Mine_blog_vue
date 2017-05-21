c2317822faff18478dc5b8e6f3964eb4a4641b74
XJH <598056288@qq.com>
2017-05-21 17:32:14 +0800

    import jquery and pie-chart

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,35 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view></router-view>
+    <div id="demo-pie-1" class="pie-title-center" data-percent="75"><span class="pie-value"></span></div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  export default {
+    name: 'app',
+    mounted () {
+      $("body").css("background", "black");
+      $('#demo-pie-1').pieChart({
+        barColor: '#10A7AF',
+        trackColor: '#fff',
+        lineCap: 'round',
+        lineWidth: 8,
+        onStep: function (from, to, percent) {
+          $(this.element).find('.pie-value').text(Math.round(percent) + '%');
+        }
+      });
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
