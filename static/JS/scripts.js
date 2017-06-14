707e25c3a693fd746dc7ce0b10150267bbd9143f
XJH <598056288@qq.com>
2017-06-14 19:54:06 +0800

    添加展示图片

--- static/JS/scripts.js
+++ static/JS/scripts.js
@@ -5,7 +5,7 @@
 
     $("html").niceScroll({styler:"fb",cursorcolor:"#CCCCCC", cursorwidth: '6', cursorborderradius: '10px', background: 'rgb(245,245,245)', spacebarenabled:false, cursorborder: '0',  zindex: '1000'});
 
-    
+
 
 })(jQuery);
 
